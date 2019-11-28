627c3b2da5f0237c00192ff5b27f469d44aba673
Amadeus Glöckner <dajg1995@gmail.com>
2019-11-28 19:51:02 +0100

    some refactoring

--- source/code/main.ts
+++ source/code/main.ts
@@ -168,13 +168,13 @@ function placeFace(
         x: number,
         y: number,
         color: string,
-        classes: string[] = []) {
+        pll: boolean) {
     const sticker = document.createElement('div');
     sticker.className = 'cubeFace';
     sticker.classList.add(color);
-    classes.forEach(theClass => {
-        sticker.classList.add(theClass);
-    });
+    if (!pll) {
+        sticker.classList.add('oll');
+    }
     sticker.style.gridColumnStart = x.toString();
     sticker.style.gridRowStart = y.toString();
     vis.appendChild(sticker);
@@ -239,55 +239,58 @@ function drawArrows(vis: HTMLDivElement, cube: any): void {
     drawArrow(svg, cube, [FACES.TOP, FACES.FRONT]);
 }
 
+function drawCube(anchor: HTMLDivElement, cube: any, pll: boolean): void {
+    if(pll){
+        drawArrows(anchor, cube);
+    }
+
+    let outputFaces = cube.getFacesArray();
+    
+    let leftFace = outputFaces[1];
+    placeFace(anchor, 1, 2, colors[leftFace[0]], pll);
+    placeFace(anchor, 1, 3, colors[leftFace[1]], pll);
+    placeFace(anchor, 1, 4, colors[leftFace[2]], pll);
+    
+    let frontFace = outputFaces[2];
+    placeFace(anchor, 2, 5, colors[frontFace[0]], pll);
+    placeFace(anchor, 3, 5, colors[frontFace[1]], pll);
+    placeFace(anchor, 4, 5, colors[frontFace[2]], pll);
+    
+    let rightFace = outputFaces[3];
+    placeFace(anchor, 5, 2, colors[rightFace[2]], pll);
+    placeFace(anchor, 5, 3, colors[rightFace[1]], pll);
+    placeFace(anchor, 5, 4, colors[rightFace[0]], pll);
+    
+    let backFace = outputFaces[4];
+    placeFace(anchor, 2, 1, colors[backFace[2]], pll);
+    placeFace(anchor, 3, 1, colors[backFace[1]], pll);
+    placeFace(anchor, 4, 1, colors[backFace[0]], pll);
+    
+    let topFace = outputFaces[0];
+    topFace.forEach((color: number, index: number) => {
+        const x = index % 3 + 2
+        const y = Math.floor(index / 3) + 2;
+        placeFace(anchor, x, y, colors[color], pll);
+    });
+}
+
 window.onload = function() {
     data.steps.forEach((step: any, stepIndex: number) => {
-        const classes = [step.short];
         step.algorithmGroups.forEach((group: any, groupIndex: number) => {
             group.algorithms.forEach(
                     (algorithm: any, algorithmIndex: number) => {
-                const id = stepIndex + '-' + groupIndex + '-' + algorithmIndex;
-
-                const vis = document
-                    .getElementById('cubeVis-' + id) as HTMLDivElement;
+                const id =
+                    `cubeVis-${stepIndex}-${groupIndex}-${algorithmIndex}`;
+                const vis = document.getElementById(id) as HTMLDivElement;
                 if(!vis){
                     return;
                 }
-                let cubeModel = CubeModel.create(faces);
 
+                let cubeModel = CubeModel.create(faces);
                 const moves = algorithm.algorithm;
                 applyMoves(cubeModel, invertMoves(moves));
-                if(step.short == 'pll'){
-                    drawArrows(vis, cubeModel);
-                }
 
-                let outputFaces = cubeModel.getFacesArray();
-                
-                let leftFace = outputFaces[1];
-                placeFace(vis, 1, 2, colors[leftFace[0]], classes);
-                placeFace(vis, 1, 3, colors[leftFace[1]], classes);
-                placeFace(vis, 1, 4, colors[leftFace[2]], classes);
-                
-                let frontFace = outputFaces[2];
-                placeFace(vis, 2, 5, colors[frontFace[0]], classes);
-                placeFace(vis, 3, 5, colors[frontFace[1]], classes);
-                placeFace(vis, 4, 5, colors[frontFace[2]], classes);
-                
-                let rightFace = outputFaces[3];
-                placeFace(vis, 5, 2, colors[rightFace[2]], classes);
-                placeFace(vis, 5, 3, colors[rightFace[1]], classes);
-                placeFace(vis, 5, 4, colors[rightFace[0]], classes);
-                
-                let backFace = outputFaces[4];
-                placeFace(vis, 2, 1, colors[backFace[2]], classes);
-                placeFace(vis, 3, 1, colors[backFace[1]], classes);
-                placeFace(vis, 4, 1, colors[backFace[0]], classes);
-                
-                let topFace = outputFaces[0];
-                topFace.forEach((color: number, index: number) => {
-                    const x = index % 3 + 2
-                    const y = Math.floor(index / 3) + 2;
-                    placeFace(vis, x, y, colors[color], classes);
-                });
+                drawCube(vis, cubeModel, step.short == 'pll');
             })
         })
     })
