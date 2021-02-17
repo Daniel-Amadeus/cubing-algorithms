29fb35a9b17b30df65dc7c7a901bfa27c1038554
Daniel-Amadeus <dajg1995@gmail.com>
2021-02-17 17:15:27 +0100

    removed build

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'build'),
         library: undefined,
         libraryTarget: 'umd'
     },
